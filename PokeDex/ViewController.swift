b9c36cec8b61f0d7963e9ea32404bb9a79451d9f
Marc Bollon <marc@infocovery.com>
2017-03-31 21:49:39 -0500

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Marc Bollon on 3/31/17.
+//  Copyright © 2017 Marc Bollon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

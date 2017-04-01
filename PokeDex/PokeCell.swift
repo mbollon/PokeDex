3175cf9db41c519693cd405c78202f0b8a0dc2cf
Marc Bollon <marc@infocovery.com>
2017-04-01 11:42:18 -0500

    Created outlets

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Marc Bollon on 3/31/17.
+//  Copyright © 2017 Marc Bollon. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
